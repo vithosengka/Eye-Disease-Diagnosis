d57f53eaab470fa3af48bb4e4439e4a2fe62be39
Vito Sengka <73872762+vithosengka@users.noreply.github.com>
2023-12-03 11:52:45 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -36,6 +36,9 @@ def diagnosis_mata(gejala_terpilih):
 
 # Tombol "Diagnosa"
 if st.button('Diagnosa'):
+    # Periksa nilai gejala_terpilih setelah tombol "Diagnosa" diklik
+    st.write('Nilai gejala_terpilih:', gejala_terpilih)
+    
     hasil_diagnosis_mata = diagnosis_mata(gejala_terpilih)
     st.subheader('Hasil Diagnosis:')
     if hasil_diagnosis_mata:
