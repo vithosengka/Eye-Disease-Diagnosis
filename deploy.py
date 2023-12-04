0a395f47a2bcad73a6a5d83a8819a03c6a216ea7
Vito Sengka <73872762+vithosengka@users.noreply.github.com>
2023-12-04 13:26:48 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,36 +1,29 @@
 import streamlit as st
 
-# Definisikan Basis Pengetahuan
 kerusakan = {
-"Motherboard Rusak": ["Komputer tidak menyala", "Kipas tidak berputar","Beberapa port USB tidak bekerja"],
+"Motherboard Rusak": ["Komputer tidak menyala", "Kipas tidak berputar","Beberapa port USB tidak bekerja"],  
 
 "VGA Rusak":["Layar hitam saat booting","Artefak/garis pada layar"],
 
 "RAM Rusak": ["BSOD", "Program crash", "Komputer lambat"],
 
-"HDD Rusak":["Boot looping","Blue screen", "Suara click pada HDD"], 
+"HDD Rusak":["Boot looping","Blue screen", "Suara click pada HDD"],
 
-"Overheating":["Komputer shutdown sendiri","PC Fans berputar kencang"]  
+"Overheating":["Komputer shutdown sendiri","PC Fans berputar kencang"]   
 }
 
-# List semua gejala  
-semua_gejala = [gejala for penyebab in kerusakan.values() for gejala in penyebab]
+semua_gejala = [gejala for gejala_list in kerusakan.values() for gejala in gejala_list]
 
-st.header("Sistem Pakar Diagnosis Kerusakan Komputer")
+st.header("Sistem Pakar Diagnosis Kerusakan PC")
+pilihan_gejala = st.multiselect("Pilih gejala yang dialami:", semua_gejala)
 
-selected = st.multiselect("Pilih gejala yang dialami", semua_gejala)  
+diagnosis = []
+for penyebab, gejala in kerusakan.items():
+    if set(pilihan_gejala) & set(gejala):  
+        diagnosis.append(penyebab)
 
-if st.button("Diagnosa"):
-  hasil_diagnosa = []
-  
-  for penyebab, gejala in kerusakan.items():
-    if set(selected).issubset(set(gejala)):
-      hasil_diagnosa.append(penyebab)  
-
-  if hasil_diagnosa:
-    st.subheader("Hasil Diagnosa")
-    st.write(f"Kemungkinan Kerusakan: {', '.join(hasil_diagnosa)}") 
-    st.write("Rekomendasi: Bawa ke teknisi komputer untuk pengecekan lebih lanjut")
-  
-  else:
-    st.write("Maaf, sistem tidak dapat menemukan kerusakan")
+hasil = ", ".join(diagnosis)  
+if hasil:
+    st.write(f"Kemungkinan kerusakan: {hasil}")
+else: 
+    st.write("Maaf, tidak ditemukan kerusakan")
