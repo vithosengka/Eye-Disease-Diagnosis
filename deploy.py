355b7352647cf01c17f0f34365d8e19460f5e62e
Vito Sengka <73872762+vithosengka@users.noreply.github.com>
2023-12-11 13:19:27 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
+# Fungsi untuk menghitung Certainty Factor
+def hitung_cf(jumlah_gejala, jumlah_cocok):
+    if jumlah_cocok == 0:
+        return 0
+
+    return jumlah_cocok / len(pilihan_gejala)
+
 st.set_page_config(page_title="Sistem Pakar Diagnosis PC")
 st.header("Sistem Pakar Diagnosis Kerusakan Komputer")
 
-# Tambah inputan pengguna
-nama = st.text_input("Nama:")
-alamat = st.text_input("Alamat:")
-nomor_hp = st.text_input("Nomor HP:")
-
+# Data Basis Pengetahuan
 kerusakan = {
     "Motherboard Rusak": ["Komputer tidak menyala", "Kipas tidak berputar", "Beberapa port USB tidak berkerja"],
     "VGA Rusak": ["Layar hitam saat booting", "Artefak/garis pada layar"],
@@ -30,61 +33,42 @@ rekomendasi = {
     "Sound Card Rusak": "Ganti sound card USB/PCI, gunakan headphone/speaker USB"
 }
 
-urutan_gejala = ["Komputer tidak menyala", "Layar hitam", "BSOD", "Program crash", "Boot looping", "Suara klik HDD"]
+# Pengumpulan Informasi
+nama = st.text_input("Nama:")
+alamat = st.text_input("Alamat:")
+nomor_hp = st.text_input("Nomor HP:")
 
-def hitung_cf(jumlah_gejala, jumlah_cocok):
-    if jumlah_cocok == 0:
-        return 0
-
-    return jumlah_cocok / len(pilihan_gejala)
-
+# Pemilihan Gejala
 semua_gejala = [gejala for gejala_list in kerusakan.values() for gejala in gejala_list]
-
 pilihan_gejala = st.multiselect("Pilih gejala yang dialami:", semua_gejala)
 
-if st.button("Diagnosis Kerusakan"):
+# Perhitungan Certainty Factor (CF) dan Prioritas Kerusakan
+ambang_cf = 0.3
+diagnosa_ditemukan = False
+jenis_kerusakan = ""
+rekomendasi_perbaikan = ""
 
-    ambang_cf = 0.3
-    diagnosa_ditemukan = False
-    jenis_kerusakan = ""
-    rekomendasi_perbaikan = ""
+for penyebab, gejala in kerusakan.items():
+    cf = hitung_cf(len(gejala), len(set(gejala) & set(pilihan_gejala)))
+    if cf >= ambang_cf and cf > diagnosa_ditemukan:
+        diagnosa_ditemukan = cf
+        jenis_kerusakan = penyebab
+        rekomendasi_perbaikan = rekomendasi[penyebab]
 
-    # Prioritas kepada gejala kerusakan "Motherboard Rusak"
-    gejala_motherboard = kerusakan["Motherboard Rusak"]
-    cf_motherboard = hitung_cf(len(gejala_motherboard), len(set(gejala_motherboard) & set(pilihan_gejala)))
+# Hasil Diagnosis dan Rekomendasi
+if diagnosa_ditemukan:
+    st.write("---")
+    st.subheader("Profil Pengguna:")
+    st.write(f"Nama: {nama}")
+    st.write(f"Alamat: {alamat}")
+    st.write(f"Nomor HP: {nomor_hp}")
 
-    if cf_motherboard >= ambang_cf:
-        diagnosa_ditemukan = True
-        jenis_kerusakan = "Motherboard Rusak"
-        rekomendasi_perbaikan = rekomendasi["Motherboard Rusak"]
-
-    # Jika tidak ada diagnosa untuk motherboard, coba kerusakan "Power Supply Rusak"
-    if not diagnosa_ditemukan:
-        gejala_power_supply = kerusakan["Power Supply Rusak"]
-        cf_power_supply = hitung_cf(len(gejala_power_supply), len(set(gejala_power_supply) & set(pilihan_gejala)))
-
-        if cf_power_supply >= ambang_cf:
-            diagnosa_ditemukan = True
-            jenis_kerusakan = "Power Supply Rusak"
-            rekomendasi_perbaikan = rekomendasi["Power Supply Rusak"]
-
-    if diagnosa_ditemukan:
-        # Tampilkan profil pengguna dan hasil diagnosis
-        st.write("---")
-        st.subheader("Profil Pengguna:")
-        st.write(f"Nama: {nama}")
-        st.write(f"Alamat: {alamat}")
-        st.write(f"Nomor HP: {nomor_hp}")
-
-        st.write("---")
-        st.subheader("Hasil Diagnosis:")
-        st.write(f"Jenis Kerusakan: {jenis_kerusakan}")
-        st.write(f"Rekomendasi Perbaikan: {rekomendasi_perbaikan}")
-
+    st.write("---")
+    st.subheader("Hasil Diagnosis:")
+    st.write(f"Jenis Kerusakan: {jenis_kerusakan}")
+    st.write(f"Rekomendasi Perbaikan: {rekomendasi_perbaikan}")
+else:
+    if set(pilihan_gejala) - set(semua_gejala):
+        st.write("Maaf, beberapa gejala yang Anda pilih tidak dikenali oleh sistem")
     else:
-        if set(pilihan_gejala) - set(semua_gejala):
-            st.write("Maaf, beberapa gejala yang Anda pilih tidak dikenali oleh sistem")
-        else:
-            st.write("Maaf, tidak ditemukan kerusakan")
-
-# ...
+        st.write("Maaf, tidak ditemukan kerusakan")
