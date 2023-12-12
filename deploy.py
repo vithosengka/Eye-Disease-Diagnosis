87d5bb7240bebe6da76059a972bab17b75b6e1a2
Vito Sengka <73872762+vithosengka@users.noreply.github.com>
2023-12-12 23:20:41 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,72 +1,100 @@
 import streamlit as st
 
-st.set_page_config(page_title="Sistem Pakar Diagnosis PC")
+# Definisikan basis pengetahuan
+gejala = [
+    "Komputer tidak menyala",
+    "Kipas tidak berputar",
+    "Beberapa port USB tidak berkerja",
+    "Layar hitam saat booting",
+    "Artefak/garis pada layar",
+    "BSOD (Blue Screen of Death)",
+    "Program crash",
+    "Komputer lambat",
+    "Boot looping",
+    "Blue screen",
+    "Suara click pada HDD",
+    "Komputer shutdown sendiri",
+    "PC Fans berputar kencang",
+    "Restart sendiri",
+    "Layar berkedip",
+    "Freeze",
+    "Tidak ada suara",
+    "Suara kotor",
+    "Noise pada speaker",
+]
 
-st.header("Sistem Pakar Diagnosis Kerusakan Komputer")
+bobot_gejala = [
+    5, 4, 3, 5, 4, 5, 4, 3, 5, 5, 5, 5, 4, 5, 4, 5, 5, 5, 4,
+]
 
-# Tambah inputan pengguna
-nama = st.text_input("Nama:")
-alamat = st.text_input("Alamat:")
-nomor_hp = st.text_input("Nomor HP:")
+diagnosa = [
+    "Motherboard Rusak",
+    "VGA Rusak",
+    "RAM Rusak",
+    "HDD Rusak",
+    "Overheating",
+    "Power Supply Rusak",
+    "Processor Rusak",
+    "Sound Card Rusak",
+]
 
-kerusakan = {
-    "Motherboard Rusak": ["Komputer tidak menyala", "Kipas tidak berputar", "Beberapa port USB tidak berkerja"],
-    "VGA Rusak": ["Layar hitam saat booting", "Artefak/garis pada layar"],
-    "RAM Rusak": ["BSOD", "Program crash", "Komputer lambat"],
-    "HDD Rusak": ["Boot looping", "Blue screen", "Suara click pada HDD"],
-    "Overheating": ["Komputer shutdown sendiri", "PC Fans berputar kencang"],
-    "Power Supply Rusak": ["Komputer tidak menyala", "Restart sendiri", "Layar berkedip"],
-    "Processor Rusak": ["Bluescreen", "Freeze", "Restart sendiri"],
-    "Sound Card Rusak": ["Tidak ada suara", "Suara kotor", "Noise pada speaker"]
-}
+rekomendasi_perbaikan = [
+    "Ganti motherboard atau bawa ke service center",
+    "Bersihkan VGA dari debu, ganti VGA, atau bawa ke service center",
+    "Ganti RAM dengan yang baru, cocokkan spesifikasinya",
+    "Ganti HDD atau clone ulang data ke HDD baru",
+    "Bersihkan CPU fan, heatsink, aplikasikan thermal paste pada CPU",
+    "Ganti power supply dengan yang baru dan sesuai kebutuhan",
+    "Ganti processor atau bawa ke service center",
+    "Ganti sound card USB/PCI, gunakan headphone/speaker USB",
+]
 
-rekomendasi = {
-    "Motherboard Rusak": "Ganti motherboard atau bawa ke service center",
-    "VGA Rusak": "Ganti VGA atau bersihkan dari debu",
-    "RAM Rusak": "Ganti RAM dengan yang baru, cocokkan spesifikasinya",
-    "HDD Rusak": "Ganti HDD atau clone ulang data ke HDD baru",
-    "Overheating": "Bersihkan CPU fan, heatsink, aplikasikan thermal paste pada CPU",
-    "Power Supply Rusak": "Ganti power supply dengan yang baru dan sesuai kebutuhan",
-    "Processor Rusak": "Ganti processor atau bawa ke service center",
-    "Sound Card Rusak": "Ganti sound card USB/PCI, gunakan headphone/speaker USB"
-}
+# Definisikan rule
+rule = [
+    ["Komputer tidak menyala", "Kipas tidak berputar", "Beberapa port USB tidak berkerja"],  # Rule untuk Motherboard Rusak
+    ["Layar hitam saat booting", "Artefak/garis pada layar"],  # Rule untuk VGA Rusak
+    ["BSOD (Blue Screen of Death)", "Program crash", "Komputer lambat"],  # Rule untuk RAM Rusak
+    ["Boot looping", "Blue screen", "Suara click pada HDD"],  # Rule untuk HDD Rusak
+    ["Komputer shutdown sendiri", "PC Fans berputar kencang"],  # Rule untuk Overheating
+    ["Komputer tidak menyala", "Restart sendiri", "Layar berkedip"],  # Rule untuk Power Supply Rusak
+    ["Bluescreen", "Freeze", "Restart sendiri"],  # Rule untuk Processor Rusak
+    ["Tidak ada suara", "Suara kotor", "Noise pada speaker"],  # Rule untuk Sound Card Rusak
+]
 
-# Ambil semua gejala dari setiap jenis kerusakan
-semua_gejala = [gejala for gejala_list in kerusakan.values() for gejala in gejala_list]
-
-# Pilih gejala yang dialami oleh pengguna
-pilihan_gejala = st.multiselect("Pilih gejala yang dialami:", semua_gejala)
-
-# Tampilkan tombol "Diagnosis Kerusakan"
-if st.button("Diagnosis Kerusakan"):
-    # Inisialisasi variabel diagnosa dan rekomendasi
-    diagnosa_ditemukan = False
-    jenis_kerusakan = ""
-    rekomendasi_perbaikan = ""
-
-    # Iterasi melalui setiap jenis kerusakan
-    for jenis, gejala in kerusakan.items():
-        cf = len(set(gejala) & set(pilihan_gejala)) / len(pilihan_gejala)
-        if cf >= 0.2:  # Ambang CF
-            diagnosa_ditemukan = True
-            jenis_kerusakan = jenis
-            rekomendasi_perbaikan = rekomendasi[jenis]
-            break  # Keluar dari perulangan jika diagnosa ditemukan
-
-    # Tampilkan hasil diagnosis
-    st.write("---")
-    st.subheader("Profil Pengguna:")
-    st.write(f"Nama: {nama}")
-    st.write(f"Alamat: {alamat}")
-    st.write(f"Nomor HP: {nomor_hp}")
-
-    if diagnosa_ditemukan:
-        st.write("---")
-        st.subheader("Hasil Diagnosis:")
-        st.write(f"Jenis Kerusakan: {jenis_kerusakan}")
-        st.write(f"Rekomendasi Perbaikan: {rekomendasi_perbaikan}")
+# Fungsi untuk memeriksa gejala
+def cek_gejala(gejala, bobot_gejala, jawaban):
+    if jawaban == "ya":
+        return bobot_gejala[gejala]
     else:
-        if set(pilihan_gejala) - set(semua_gejala):
-            st.write("Maaf, beberapa gejala yang Anda pilih tidak dikenali oleh sistem")
-        else:
-            st.write("Maaf, tidak ditemukan kerusakan")
+        return 0
+
+# Fungsi untuk menghitung kemungkinan kerusakan
+def hitung_kemungkinan(selected_gejala, bobot_gejala):
+    kemungkinan = [0] * len(diagnosa)
+    for i, rule_gejala in enumerate(rule):
+        for j, gejala in enumerate(selected_gejala):
+            if gejala in rule_gejala:
+                kemungkinan[i] += cek_gejala(j, bobot_gejala, "ya")
+    return kemungkinan
+
+# Fungsi untuk mendapatkan diagnosa
+def get_diagnosa(kemungkinan, diagnosa):
+    index_tertinggi = kemungkinan.index(max(kemungkinan))
+    return diagnosa[index_tertinggi], rekomendasi_perbaikan[index_tertinggi]
+
+# Tampilkan checkbox untuk gejala
+selected_gejala = st.multiselect("Pilih gejala yang dialami:", gejala)
+
+# Tampilkan tombol untuk diagnosis
+if st.button("Diagnosis Kerusakan"):
+    # Hitung kemungkinan kerusakan
+    kemungkinan_kerusakan = hitung_kemungkinan(selected_gejala, bobot_gejala)
+
+    # Dapatkan diagnosa
+    hasil_diagnosa, rekomendasi = get_diagnosa(kemungkinan_kerusakan, diagnosa)
+
+    # Tampilkan hasil
+    st.write("---")
+    st.subheader("Hasil Diagnosis:")
+    st.write(f"Jenis Kerusakan: {hasil_diagnosa}")
+    st.write(f"Rekomendasi Perbaikan: {rekomendasi}")
