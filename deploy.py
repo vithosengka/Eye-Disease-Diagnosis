75c4642f28021ec14049d43a58b5fabd2b2523e3
Vito Sengka <73872762+vithosengka@users.noreply.github.com>
2023-12-12 19:16:31 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Sistem Pakar Diagnosis PC")
+
 st.header("Sistem Pakar Diagnosis Kerusakan Komputer")
 
 # Tambah inputan pengguna
@@ -30,61 +31,42 @@ rekomendasi = {
     "Sound Card Rusak": "Ganti sound card USB/PCI, gunakan headphone/speaker USB"
 }
 
-urutan_gejala = ["Komputer tidak menyala", "Layar hitam", "BSOD", "Program crash", "Boot looping", "Suara klik HDD"]
-
-def hitung_cf(jumlah_gejala, jumlah_cocok):
-    if jumlah_cocok == 0:
-        return 0
-
-    return jumlah_cocok / len(pilihan_gejala)
-
+# Ambil semua gejala dari setiap jenis kerusakan
 semua_gejala = [gejala for gejala_list in kerusakan.values() for gejala in gejala_list]
 
+# Pilih gejala yang dialami oleh pengguna
 pilihan_gejala = st.multiselect("Pilih gejala yang dialami:", semua_gejala)
 
+# Tampilkan tombol "Diagnosis Kerusakan"
 if st.button("Diagnosis Kerusakan"):
-
-    ambang_cf = 0.3
+    # Inisialisasi variabel diagnosa dan rekomendasi
     diagnosa_ditemukan = False
     jenis_kerusakan = ""
     rekomendasi_perbaikan = ""
 
-    # Prioritas kepada gejala kerusakan "Motherboard Rusak"
-    gejala_motherboard = kerusakan["Motherboard Rusak"]
-    cf_motherboard = hitung_cf(len(gejala_motherboard), len(set(gejala_motherboard) & set(pilihan_gejala)))
-
-    if cf_motherboard >= ambang_cf:
-        diagnosa_ditemukan = True
-        jenis_kerusakan = "Motherboard Rusak"
-        rekomendasi_perbaikan = rekomendasi["Motherboard Rusak"]
-
-    # Jika tidak ada diagnosa untuk motherboard, coba kerusakan "Power Supply Rusak"
-    if not diagnosa_ditemukan:
-        gejala_power_supply = kerusakan["Power Supply Rusak"]
-        cf_power_supply = hitung_cf(len(gejala_power_supply), len(set(gejala_power_supply) & set(pilihan_gejala)))
-
-        if cf_power_supply >= ambang_cf:
+    # Iterasi melalui setiap jenis kerusakan
+    for jenis, gejala in kerusakan.items():
+        cf = len(set(gejala) & set(pilihan_gejala)) / len(pilihan_gejala)
+        if cf >= 0.3:  # Ambang CF
             diagnosa_ditemukan = True
-            jenis_kerusakan = "Power Supply Rusak"
-            rekomendasi_perbaikan = rekomendasi["Power Supply Rusak"]
+            jenis_kerusakan = jenis
+            rekomendasi_perbaikan = rekomendasi[jenis]
+            break  # Keluar dari perulangan jika diagnosa ditemukan
+
+    # Tampilkan hasil diagnosis
+    st.write("---")
+    st.subheader("Profil Pengguna:")
+    st.write(f"Nama: {nama}")
+    st.write(f"Alamat: {alamat}")
+    st.write(f"Nomor HP: {nomor_hp}")
 
     if diagnosa_ditemukan:
-        # Tampilkan profil pengguna dan hasil diagnosis
-        st.write("---")
-        st.subheader("Profil Pengguna:")
-        st.write(f"Nama: {nama}")
-        st.write(f"Alamat: {alamat}")
-        st.write(f"Nomor HP: {nomor_hp}")
-
         st.write("---")
         st.subheader("Hasil Diagnosis:")
         st.write(f"Jenis Kerusakan: {jenis_kerusakan}")
         st.write(f"Rekomendasi Perbaikan: {rekomendasi_perbaikan}")
-
     else:
         if set(pilihan_gejala) - set(semua_gejala):
             st.write("Maaf, beberapa gejala yang Anda pilih tidak dikenali oleh sistem")
         else:
             st.write("Maaf, tidak ditemukan kerusakan")
-
-# ...
